426aa3199329e64b8e98e5c59002796e2fb7d61d
AxelBau19 <bautista.ros.naranja.2017@gmail.com>
2025-09-23 17:18:17 -0600

    Add file theme dark
    Maybe this has problem

--- personal-web/src/App.jsx
+++ personal-web/src/App.jsx
@@ -1,3 +1,5 @@
+import NavBar from "./components/NavBar"
+import { ThemeProvider } from "./context/ThemeContext"
 import { Home } from "./pages/Home"
 import { NotFound } from "./pages/NotFound"
 import { BrowserRouter,Route,Routes } from "react-router-dom"
@@ -5,14 +7,15 @@ import { BrowserRouter,Route,Routes } from "react-router-dom"
 function App() {
 
   return (
-    <>
+    <ThemeProvider>
       <BrowserRouter>
+        <NavBar/>
         <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home/>}/>
+          <Route path="*" element={<NotFound/>}/>
         </Routes>
       </BrowserRouter>
-    </>
+    </ThemeProvider>
   )
 }
 
